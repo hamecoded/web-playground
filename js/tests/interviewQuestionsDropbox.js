e578600e0080ba03179488eee99345a73ceb46dd
odeds <odeds@liveperson.com>
2017-01-03 15:14:20 +0200

    add first dropbox interview question

--- js/tests/interviewQuestionsDropbox.js
+++ js/tests/interviewQuestionsDropbox.js
@@ -1,3 +1,48 @@
+/**
+ * return all elements containing a certain class name
+ *  afterwhich step 2 of the interview was:
+ *   getByClassNameByHierarchy(element, hierarchyClassName)   // a > b > c
+ * @param  {[type]} domElement [description]
+ * @param  {[type]} className  [description]
+ * @return {[type]}            Returns: [<div id="e1" />, <div id="e3" />, <div id="e5" />] 
+ */
+export function getByClassName(domElement = 'e1', className = 'a') {
+	let isId = typeof domElement === 'string';
+	if(isId){
+		domElement = document.getElementById(domElement);
+	}
+    let arr = _getByClassName(domElement, className);
+    let output = arr.reduce(function(accumulator, currentValue) {
+	  return accumulator + currentValue.id + ',';
+	}, '');
+
+    return {
+		description: 'First Interview: implement getByClassName then getByClassNameByHierarchy',
+		value: output
+	};
+}
+
+function _getByClassName(domElement, className) {
+    let output = [];
+    if(domElement.classList.contains(className)){
+      output.push(domElement);
+    }
+    for ( let el of domElement.children) {
+        output = output.concat(_getByClassName(el, className));
+    }
+    return output;
+}
+
+///////////////////////////////////////////////////////////
+
+/**
+ * given an element return it's DOM Equality signature
+ * 
+ * @param  {[type]} el         element to inspect
+ * @param  {[type]} descendant a recursion backtracing Object that would eventally get returned
+ * @return {[type]}            return it's DOM Equality signature, 
+ *                              eg: ['SPAN':1,'DIV':1,'IMG':2]
+ */
 function inspectDom(el, descendant) {
 	descendant = descendant || {};
 	for(let child of el.children){		
@@ -34,10 +79,37 @@ export function areEqual() {
 	let el1Inspection = inspectDom(element1), el2Inspection = inspectDom(element2);
 
 	return {
-		description: 'Question 1: DOM Equality - DOM Nodes are “DOM Equal” if they have the same number of descendant elements of every type.',
+		description: 'Question 1-1: DOM Equality - DOM Nodes are “DOM Equal” if they have the same number of descendant elements of every type.',
 		value: isEqual(el1Inspection,el2Inspection)
 	}
 	// Note: JSON.stringify(el1Inspection) === JSON.stringify(el2Inspection)
 	// does not promise order hence unreliable
 }
+
+///////////////////////////////////////////////////////////
+
+/**
+ * Given an element, please implement the following function:
+ *  function equalElements(element)
+ * Which returns all the equality groups.
+ * Equality group is defined as a list of all descendant elements which are DOM Equal
+ *  [[elem1, elem2], ...]
+ * @param  {[type]} element [description]
+ * @return {[type]}         [description]
+ */
+export function equalElements(element) {
+	for(let child of element.children){		
+		descendant[child.nodeName] = descendant[child.nodeName] ? ++descendant[child.nodeName] : 1;
+		if(child.children.length > 0){
+			inspectDom(child, descendant);
+		}
+	}
+	return {
+		description: 'Question 1-2: returns all the equality groups descendant off given element',
+		value: 1
+	}
+}
+
+
+///////////////////////////////////////////////////////////
 
