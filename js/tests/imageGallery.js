b0ca9823e646117082701bacc8057ab55c4aee1c
Oded Sagir <odedsagir@gmail.com>
2017-01-09 20:34:17 +0200

    dropbox interview day questions

--- js/tests/imageGallery.js
+++ js/tests/imageGallery.js
@@ -5,7 +5,7 @@ var imgThumbs = ['http://placehold.it/400x400/f0d/f',
 'http://placehold.it/350x300/fd0/f',
 'http://placehold.it/200x300/9d0/f'];
 var imgFull = [
-'http://placehold.it/800x800/f0d/f',
+'http://placehold.it/300x300/f0d/f',
 'http://placehold.it/600x700/00d/f',
 'http://placehold.it/600x400/0df/f',
 'http://placehold.it/700x600/fd0/f',
@@ -14,14 +14,20 @@ var imgFull = [
 var myGallery, thumbnails, largePreview;
 
 function setSelected (el) {
-  var ls = thumbnails.querySelectorAll('.thumb');
-  ls.forEach(function(thumb){
-    thumb.classList.remove('selected');
-  })
-  el.classList.add('selected');
+	var ls = thumbnails.querySelectorAll('.thumb');
+	ls.forEach(function(thumb){
+		thumb.classList.remove('selected');
+	});
+	el.classList.add('selected');
+	var i = el.dataset.i;
+	//  TODO: reuse large email rather than regenerate by hiding the next time or prerendering all
+	var img = new Image();
+	img.src = imgFull[i];
+	largePreview.innerHTML = "";
+	largePreview.appendChild(img);
 }
 
-
+// requirements: thumbs are original size and clipped in the center of the 100by100 container
 export function imageGallery () {
 	myGallery = document.getElementById('myGallery');
 	thumbnails = myGallery.querySelector('.thumbnails');
@@ -33,22 +39,14 @@ export function imageGallery () {
 	  img.src = url;
 	  el.appendChild(img);
 	  el.classList.add('thumb');
-	  if(index === 0){
-	      el.classList.add('selected');
-	  }
-	  
 	  el.dataset.i = index;
-	  
 	  thumbnails.appendChild(el);
-	  
+
+	  if(index === 0){
+	    setSelected(el);
+	  }
 	  el.addEventListener('click', function(event){
-		var i = el.dataset.i;
-		var img = new Image();
-		img.src = imgFull[i];
-		largePreview.innerHTML = "";
-		largePreview.appendChild(img);
 		setSelected(el);
-	    //event.stopPropagation();
 	  });
 	});
 
