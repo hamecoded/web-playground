ce05399274255b9b487d7236e35247a1cedc7037
Oded Sagir <odedsagir@gmail.com>
2017-06-19 11:21:25 +0300

    add animation to the tree of life

--- js/tests/treeOfLife.js
+++ js/tests/treeOfLife.js
@@ -12,16 +12,20 @@
 
 var context;
 const radius = 50;
+var timeGap= 0;
 
-function draw (point, fill, r) {
-	context.beginPath();
-	context.fillStyle = fill || 'white';
-	//context.strokeStyle = 'black';
+function draw (point, stroke, fill, r, gap) {
+	timeGap += gap !== undefined || 100;
+	_.delay(() => {
+		context.beginPath();
+		context.fillStyle = fill || 'white';
+		context.strokeStyle = stroke || 'black';
 
-	context.arc(point.x, point.y, r || radius, 0, 2 * Math.PI, false);
-	context.lineWidth = 2;
-    context.stroke();
-    fill && context.fill();
+		context.arc(point.x, point.y, r || radius, 0, 2 * Math.PI, false);
+		context.lineWidth = 2;
+	    context.stroke();
+	    fill && context.fill();
+	}, timeGap);
 }
 
 export function treeOfLife () {
@@ -29,37 +33,40 @@ export function treeOfLife () {
 	context = canvas.getContext('2d');
 	context.save();
 	
-	//center circle
+	// center circle
 	var c1 = new Point2D(canvas.width / 2, canvas.height / 2);
-	context.strokeStyle = 'orange';
-	draw(c1, 'rgba(0, 0, 0, 0.5)');
+	draw(c1, 'orange');
 
-	// the tree of life encompassing circle
-	context.strokeStyle = 'black';
-	draw(c1, 'rgba(0, 0, 0, 0.2)', radius*3);
-
-
+	// tier 2 circles
 	//first circle above central circle
-	context.strokeStyle = 'red';
     var c2 = new Point2D(c1.x, c1.y-radius);
-	draw(c2);
+	draw(c2, 'red');
 	//the rest of tier 2 looping circles
 	var arrTier2= [c2];
 	var i = 5, inter = Intersection.intersectCircleCircle(c1,radius,c2,radius);
 	while(inter.points.length > 0 && i--) {
 		let cn = inter.points[0];
 		arrTier2.push(cn);
-		draw(cn);
+		draw(cn, 'red');
 		inter = Intersection.intersectCircleCircle(c1,radius,cn,radius);
 	}
 
 	var arrTier3= nextTier('green ', arrTier2);
-	var arrTier4= nextTier('pink', arrTier3, undefined, undefined, 'rgba(0, 0, 0, 0.5)');
+	var arrTier4= nextTier('pink', arrTier3);
+
+	// the tree of life encompassing circle
+	draw(c1, 'black', 'rgba(0, 0, 0, 0.2)', radius*3);
+	
+	// the outer circles
 	var arrTier5= nextTier('blue', arrTier4, arrTier3);
 	var arrTier6= nextTier('yellow', arrTier5,undefined,11);
 	var arrTier7= nextTier('red', arrTier6,undefined,11);
 	//var arrTier8= nextTier('gray', arrTier7,arrTier6,11);
 	//var arrTier9= nextTier('brown', arrTier8,undefined,23);
+
+	// marking the fruit of life
+	draw(c1, 'black', 'rgba(0, 0, 0, 0.5)', 0); //first circle fill
+	nextTier('black', arrTier3, undefined, undefined, 'rgba(0, 0, 0, 0.5)', 0); //fill for the fruit of life middle tier
 	var arrFinalize= finalize('black', arrTier7); //the fruit of life completion
 
 	return {
@@ -74,13 +81,13 @@ function finalize (color, prevTier){
 	for(let i = 0 ; i < 12;) {
 		let cn = Intersection.intersectCircleCircle(prevTier[++i],radius,prevTier[++i%12],radius).points[1];
 		retTier.push( cn );
-		draw(cn, 'rgba(0, 0, 0, 0.5)');
+		draw(cn, 'black','rgba(0, 0, 0, 0.5)',undefined, 0);
 	}
 
 	return retTier;
 }
 
-function nextTier (color, prevTier, prev2Tier, count, fill) {
+function nextTier (color, prevTier, prev2Tier, count, fill, gap) {
   	context.strokeStyle = color;
 	var retTier= [];
 	count = count || 5;
@@ -89,17 +96,17 @@ function nextTier (color, prevTier, prev2Tier, count, fill) {
 		for(let i = 0; i < count; ){
 			let cn = Intersection.intersectCircleCircle(prevTier[i],radius,prev2Tier[i],radius).points[0];
 			retTier.push( cn );
-			draw(cn);
+			draw(cn, color);
 			cn = Intersection.intersectCircleCircle(prevTier[i],radius,prev2Tier[++i],radius).points[1];
 			retTier.push( cn );
-			draw(cn);
+			draw(cn, color);
 		}
 		let cn = Intersection.intersectCircleCircle(prevTier[count],radius,prev2Tier[count],radius).points[0];
 		retTier.push( cn );
-		draw(cn);
+		draw(cn, color);
 		cn = Intersection.intersectCircleCircle(prev2Tier[0],radius,prevTier[count],radius).points[0];
 		retTier.push( cn );
-		draw(cn);
+		draw(cn, color);
 		return retTier;
 	}
 
@@ -107,11 +114,11 @@ function nextTier (color, prevTier, prev2Tier, count, fill) {
 	while(i < count) {
 		let cn = Intersection.intersectCircleCircle(prevTier[i],radius,prevTier[++i],radius).points[1];
 		retTier.push( cn );
-		draw(cn, fill);
+		draw(cn, color, fill, undefined, gap);
 	}
 	let cn = Intersection.intersectCircleCircle(prevTier[count],radius,prevTier[0],radius).points[1];
 	retTier.push( cn );
-	draw(cn, fill);
+	draw(cn, color, fill, undefined, gap);
 
 	return retTier;
 };
